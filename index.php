687a2ef46f3639ed0c36e5ace43744d5d9cf132d
PazitoPazos <javier.pazos001@gmail.com>
2022-01-13 14:11:38 +0100

    Array de dúas dimensións

--- index.php
+++ index.php
@@ -14,6 +14,30 @@
             // Array asociativo
             $dnisAmigos=array("Antón"=>"12345678X","Rosalía"=>"11112222X","Xoan"=>"99997777X");
             echo "O DNI de Rosalía é: ".$dnisAmigos["Rosalía"]."<br/>";
+
+            // Array de dúas dimensións
+            $cocheAmigos = array
+                (
+                    "Antón"=>array
+                    (
+                        "Fiat 500",
+                        "Audi A3",
+                        "Peugeot 208"
+                    ),
+                    "Rosalía"=>array
+                    (
+                        "Audi TT"
+                    ),
+                    "Xoan"=>array
+                    (
+                        "Jaguar XF",
+                        "BMW 520d",
+                        "Seat Ibiza"
+                    )
+                );
+            
+            echo "O segundo coche de Xoan é ".$cochesAmigos["Xoan"][1]."<br/>";
+
         ?>
     </body>
 </html>
