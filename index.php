6c01c067ba145b957217e408e4e34cdd01535159
PazitoPazos <javier.pazos001@gmail.com>
2022-01-13 13:56:19 +0100

    Array indexado

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Definición de arrays en PHP</h3>
+
+        <?php
+            // Array indexado
+            $amigos=array("Antón","Rosalía","Xoán");
+            echo "O terceiro amigo é: ".$amigos[2]."<br/>";
+        ?>
+    </body>
+</html>
